97774ca27925aa164c9a0c1053f1fb5f64540b98
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-10-23 00:16:23 +0300

    init: add import svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+    checker({
+      overlay: { initialIsOpen: false },
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
