dc75ad42061c8aa58620933b52fd24f74832e986
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-10-26 22:06:45 +0300

    feat: add list card component to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,46 @@
 import { Component } from 'react';
 import Search from '../../components/Search/Search';
 import Section from '../../ui/Section/Section';
+import fetchData from '../../services/getItems';
+import { paramApiType } from '../../types/interface';
+import ListCards, { TListCardData } from '../../components/ListCards/ListCards';
 
-class HomePage extends Component {
-  getSearchQuery = (query: string) => {
+interface IHomePageState {
+  dataItems: TListCardData;
+}
+
+class HomePage extends Component<object, IHomePageState> {
+  private apiQuery: paramApiType[];
+
+  constructor(props: object) {
+    super(props);
+    this.apiQuery = [];
+    this.state = {
+      dataItems: [],
+    };
+  }
+
+  loadData = async (query: string) => {
+    this.setState({ dataItems: [] });
     console.log('query', query);
+    this.apiQuery = [
+      { key: 'q', value: query ? query : '*' },
+      { key: 'media_type', value: 'image' },
+      { key: 'page_size', value: 12 },
+      { key: 'page', value: 1 },
+    ];
+    const data = await fetchData(this.apiQuery);
+    console.log('loadData', data);
+    this.setState({ dataItems: data });
   };
 
   render = () => (
     <>
       <Section>
-        <Search onInputQuery={this.getSearchQuery} />
+        <Search onInputQuery={this.loadData} />
       </Section>
       <Section classNames={['section--full_height']}>
-        <div>List</div>
+        <ListCards data={this.state.dataItems} />
       </Section>
     </>
   );
