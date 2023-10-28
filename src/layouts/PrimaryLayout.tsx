35b9865d70c842ce6dc06fedab2f112fb99f34c5
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-10-28 14:16:40 +0300

    feat: add ErrorBoundary component

--- src/layouts/PrimaryLayout.tsx
+++ src/layouts/PrimaryLayout.tsx
@@ -1,8 +1,14 @@
 import { Component } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+
 class PrimaryLayout extends Component {
-  render = () => <Outlet />;
+  render = () => (
+    <ErrorBoundary>
+      <Outlet />;
+    </ErrorBoundary>
+  );
 }
 
 export default PrimaryLayout;
