c945f60ac5853b1fe2ec0dfff9bf916ece7491c7
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-12-01 15:39:59 +0200

    feat: add FiledText component

---
+++ src/ui/UnControll/FiledText/FiledText.tsx
@@ -0,0 +1,54 @@
+import { FC, InputHTMLAttributes } from 'react';
+
+import Filed from '../Filed/Filed';
+
+import './FiledText.scss';
+
+interface IFiledTextProps extends InputHTMLAttributes<HTMLInputElement> {
+  inputRef: React.RefObject<HTMLInputElement>;
+  errorMessageRef: React.RefObject<HTMLElement>;
+  wrapRef: React.RefObject<HTMLDivElement>;
+  name: string;
+  id: string;
+  label?: string;
+}
+
+const FiledText: FC<IFiledTextProps> = ({
+  inputRef,
+  name,
+  id,
+  label = '',
+  errorMessageRef,
+  wrapRef,
+  ...restProps
+}) => {
+  const handleFocus = () => {
+    wrapRef.current?.classList.add('filed__wr--focus');
+  };
+
+  const handleBlur = () => {
+    wrapRef.current?.classList.remove('filed__wr--focus');
+  };
+
+  return (
+    <Filed
+      id={id}
+      label={label}
+      errorMessageRef={errorMessageRef}
+      wrapRef={wrapRef}
+    >
+      <input
+        ref={inputRef}
+        name={name}
+        id={id}
+        type="text"
+        className="filed__input"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        {...restProps}
+      />
+    </Filed>
+  );
+};
+
+export default FiledText;
